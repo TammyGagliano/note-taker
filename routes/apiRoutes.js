c51df446da84090ebe6c2f501f1f479b0d0f5807
Tammy Gagliano <tracsd@gmail.com>
2021-10-22 09:43:41 -0500

    errors have been corrected and server works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,29 @@
 const router = require('express').Router();
-const saveData = require('/notes');
+const saveData = require('../db/db.json');
 // Helper method for generating unique ids
-const uuid = require('./helper/uuid');
+// const uuid = require('./helper/uuid');
+// const util = require('./helper/util);
 
 // GET request
-router.get('./notes', function (req, res) {
-    store
+router.get('/notes', function (req, res) {
+    saveData
     .retrieveNotes();
     then(notes => res.json(notes))
     .catch(err => res.status(500).json(err))
 });
   
 // POST request
-router.post('./notes', function (req, res) {
-    store
+router.post('/notes', function (req, res) {
+    saveData
     .addNote(req.body)
     then(notes => res.json(notes))
     .catch(err => res.status(500).json(err))
 });
 
 // DELETE Request
-router.delete('./notes/:title', function (req, res) {
-    store
-    deleteNotes(req.params.title)
+router.delete('notes/:id', function (req, res) {
+    saveData
+    .deleteNotes(req.params.title)
     then(notes => res.json({ ok: true }))
     .catch(err => res.status(500).json(err))
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,13 @@ const path = require('path');
 const router = require('express').Router();
 
 // Get notes.html if the URL is localhost:3001/notes
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+router.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 // if no matching route is found default to home page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+router.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 module.exports = router;
