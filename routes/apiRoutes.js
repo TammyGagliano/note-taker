39551ac316c5c69e211ba5d6b13ef00f6ba614b9
Tammy Gagliano <tracsd@gmail.com>
2021-10-24 21:38:59 -0500

    added screenshot

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
   //Set up notes variable"
-  fs.readFile("db/db.json", "utf8", (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
 
@@ -29,19 +29,18 @@ const path = require("path");
 
     // Get notes.html if the URL is localhost:3001/notes
     router.get('/notes', (req, res) => 
-        res.sendFile(path.join(__dirname, '/public/notes.html'))
+        res.sendFile(path.join(__dirname, './public/notes.html'))
     );
 
 // if no matching route is found default to home page
     router.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
     );
     //updates the json file whenever a note is added or deleted
     function updateDb() {
       fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
         if (err) {
             console.log("error")
-            return console.log(err);
         }
         console.log("Success!");
         return true;
