5671d18785f9f81c326f99e5555fe82ae796a960
Tammy Gagliano <tracsd@gmail.com>
2021-10-24 21:32:57 -0500

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # note-taker
+## Description: 
+An application that can be used to write, save, and delete notes. This application uses express backend and saves and retrieves note data from a JSON file.
+
+## List of Technology
+- Javascript
+- HTML
+- CSS 
+- Express.js
+- Node.js
+
+## Installation 
+Clone a copy from GitHub. 
+Open project directory, then npm install to install all dependencies
+
+## Usage 
+1. node server.js
+2. navigate to localhost:3001
+3. Click "Get Started"
+4. Add a note title and any note text then press the save icon in the top right corner
+5. To create a new note, click on the pencil icon
+6. View your notes on the left hand side. 
+
+## Contributions
+Tammy Gagliano 
+https://github.com/TammyGagliano/note-taker
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-// const saveData = require('../db/saveData.js');
 
-module.exports = (router) => {
   //Set up notes variable"
   fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    let notes = JSON.parse(data);
+    var notes = JSON.parse(data);
 
     // GET request
     router.get("/notes", function (req, res) {
@@ -25,18 +23,31 @@ module.exports = (router) => {
 
     // DELETE Request
     router.delete("/api/notes/:id", function (req, res) {
-      notes.splice(req.params.id, 1);
+      notes.splice(req.params.id);
       updateDb();
     });
 
+    // Get notes.html if the URL is localhost:3001/notes
+    router.get('/notes', (req, res) => 
+        res.sendFile(path.join(__dirname, '/public/notes.html'))
+    );
+
+// if no matching route is found default to home page
+    router.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
     //updates the json file whenever a note is added or deleted
     function updateDb() {
       fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
-        if (err) throw err;
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+        console.log("Success!");
         return true;
       });
     }
   });
-};
+
 // accessing router middleware
 module.exports = router;
